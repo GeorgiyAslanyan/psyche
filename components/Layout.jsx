c9b31208b8ddd687ac01c4d6bf0ddb3a353ad642
GEORGIY ASLANYAN <13jeff07@gmail.com>
2023-10-11 16:23:56 +0500

    video fix

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -12,8 +12,12 @@ const Layout = () => {
             <Timer />
             <p className="text-center 2xl:text-2xl mt-4 md:mt-10 text-white">Psyche is supposed to have <br className='hidden md:block'/> limited depoist of Etherium <br className='hidden md:block'/>in addition to metal</p>
         </div>
-        <video autoPlay loop muted className='w-full object-cover absolute z-0 h-screen hidden md:flex' src="/fhd.mp4"></video>
-        <video autoPlay loop muted className='w-full object-cover absolute z-0 h-screen flex md:hidden' src="/mobile.mp4"></video>
+        <video autoPlay loop muted className='w-full object-cover absolute z-0 h-screen hidden md:flex'>
+            <source src="/fhd.mp4" type="video/mp4" />
+        </video>
+        <video autoPlay loop muted className='w-full object-cover absolute z-0 h-screen flex md:hidden'>
+        <source src="/mobile.mp4" type="video/mp4" />
+        </video>
         <Footer />
     </div>
   )
