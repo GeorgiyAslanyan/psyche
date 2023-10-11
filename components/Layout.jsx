89eee4fc40c66d20b350b5a14aba36bfc134caea
GEORGIY ASLANYAN <13jeff07@gmail.com>
2023-10-11 21:20:56 +0500

    text fix

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -10,7 +10,7 @@ const Layout = () => {
         <div className='absolute z-10 top-[10%] md:top-[15%] left-3 pr-3 sm:pr-0 sm:left-8'>
             <p className="text-xl md:text-3xl text-center pb-1">Countdown to launch</p>
             <Timer />
-            <p className="text-center 2xl:text-2xl mt-4 md:mt-10 text-white">Psyche is supposed to have <br className='hidden md:block'/> <span className=' text-[#AEFEFF]'>limitless deposits of Etherium</span> <br className='hidden md:block'/> in addition to metal</p>
+            <p className="text-center 2xl:text-2xl mt-4 md:mt-10 text-white">Psyche is supposed to have <br className='hidden md:block'/> <span className=' text-[#AEFEFF]'>limitless deposits of Ethereum</span> <br className='hidden md:block'/> in addition to metal</p>
         </div>
         <video autoPlay loop muted playsInline className='w-full object-cover absolute z-0 h-screen hidden md:flex'>
             <source src="/fhd.mp4" type="video/mp4" />
